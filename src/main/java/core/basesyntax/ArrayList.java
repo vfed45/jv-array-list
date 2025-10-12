389c2d032156f39d8f04a5b05b5cb3383b7a187d
Viktoriia <fvee737@gmail.com>
2025-10-12 21:31:11 +0200

    fixed2

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -38,6 +38,7 @@ public class ArrayList<T> implements List<T> {
             elementData = newArray;
         }
     }
+
     private void checkIndex(int index) {
         if (index < 0 || index >= size) {
             throw new ArrayListIndexOutOfBoundsException("Index " + index
